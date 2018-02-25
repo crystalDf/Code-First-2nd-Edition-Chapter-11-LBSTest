64f4acebe33dc7891861cd4323ccea935e6d3884
crystalDf <chendong333@gmail.com>
2018-02-25 22:54:42 +0800

    Chapter 11 LBSTest

---
+++ app/src/main/java/com/star/lbstest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.star.lbstest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.baidu.location.LocationClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    public LocationClient mLocationClient;
+
+    private TextView mPositionTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
