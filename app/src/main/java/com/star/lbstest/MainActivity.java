e47412a9d2ebe1b7a74ef0ecc47212329917db7b
crystalDf <chendong333@gmail.com>
2018-02-26 22:52:32 +0800

    Chapter 11 LBSTest

--- app/src/main/java/com/star/lbstest/MainActivity.java
+++ app/src/main/java/com/star/lbstest/MainActivity.java
@@ -13,6 +13,14 @@ import android.widget.Toast;
 import com.baidu.location.BDAbstractLocationListener;
 import com.baidu.location.BDLocation;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,16 +38,27 @@ public class MainActivity extends AppCompatActivity {
     public LocationClient mLocationClient;
 
     private TextView mPositionTextView;
+    private MapView mMapView;
+
+    private BaiduMap mBaiduMap;
+    private boolean isFirstLocate = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SDKInitializer.initialize(getApplicationContext());
+
         setContentView(R.layout.activity_main);
 
         mLocationClient = new LocationClient(getApplicationContext());
         mLocationClient.registerLocationListener(new MyLocationListener());
 
         mPositionTextView = findViewById(R.id.position_text_view);
+        mMapView = findViewById(R.id.mapView);
+
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
 
         List<String> permissionList = new ArrayList<>();
 
@@ -58,6 +77,30 @@ public class MainActivity extends AppCompatActivity {
         } else {
             requestLocation();
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mMapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        mLocationClient.stop();
+        mMapView.onDestroy();
+
+        mBaiduMap.setMyLocationEnabled(false);
     }
 
     @Override
@@ -102,7 +145,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void requestLocation() {
+        initLocation();
         mLocationClient.start();
+    }
+
+    private void initLocation() {
+        LocationClientOption locationClientOption = new LocationClientOption();
+        locationClientOption.setScanSpan(5000);
+//        locationClientOption.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);
+        locationClientOption.setIsNeedAddress(true);
+        mLocationClient.setLocOption(locationClientOption);
+    }
+
+    private void navigateTo(BDLocation bdLocation) {
+
+        if (isFirstLocate) {
+            LatLng latLng = new LatLng(bdLocation.getLatitude(), bdLocation.getLongitude());
+            MapStatusUpdate mapStatusUpdate = MapStatusUpdateFactory.newLatLng(latLng);
+            mBaiduMap.animateMapStatus(mapStatusUpdate);
+
+            mapStatusUpdate = MapStatusUpdateFactory.zoomTo(16);
+            mBaiduMap.animateMapStatus(mapStatusUpdate);
+
+            isFirstLocate = false;
+        }
+
+        MyLocationData.Builder builder = new MyLocationData.Builder();
+        builder
+                .latitude(bdLocation.getLatitude())
+                .longitude(bdLocation.getLongitude());
+
+        mBaiduMap.setMyLocationData(builder.build());
     }
 
     public class MyLocationListener extends BDAbstractLocationListener {
@@ -117,9 +190,11 @@ public class MainActivity extends AppCompatActivity {
             switch (bdLocation.getLocType()) {
                 case BDLocation.TypeGpsLocation:
                     locType = "GPS";
+                    navigateTo(bdLocation);
                     break;
                 case BDLocation.TypeNetWorkLocation:
                     locType = "Network";
+                    navigateTo(bdLocation);
                     break;
                 default:
             }
@@ -127,6 +202,11 @@ public class MainActivity extends AppCompatActivity {
             currentPosition
                     .append("Latitude: ").append(bdLocation.getLatitude()).append("\n")
                     .append("Longitude: ").append(bdLocation.getLongitude()).append("\n")
+                    .append("Country: ").append(bdLocation.getCountry()).append("\n")
+                    .append("Province: ").append(bdLocation.getProvince()).append("\n")
+                    .append("City: ").append(bdLocation.getCity()).append("\n")
+                    .append("District: ").append(bdLocation.getDistrict()).append("\n")
+                    .append("Street: ").append(bdLocation.getStreet()).append("\n")
                     .append("LocType: ").append(locType).append("\n");
 
             mPositionTextView.setText(currentPosition);
